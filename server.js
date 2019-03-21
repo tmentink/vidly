efd2742b17a33d99c36aaf5ea1af752ff4430212
Trent Mentink <trentmentink@gmail.com>
2019-03-21 12:27:39 -0700

    Added joi-objectid package
    Installed joi-objectid package and initialized it inside startup/validation.js.
    Added startup/validation.js to server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 
 require('./startup/db.js')()
 require('./startup/routes.js')(app)
+require('./startup/validation.js')()
 
 const port = process.env.PORT || config.get('port')
 const server = app.listen(port, () =>
