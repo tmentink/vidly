8009f782def0b43b26a6446a341cb403531f3564
Trent Mentink <trentmentink@gmail.com>
2019-03-21 13:05:26 -0700

    Added Rental model and route
    Created Rental model and route and included the route in startup/routes

--- startup/routes.js
+++ startup/routes.js
@@ -2,10 +2,12 @@ const express = require('express')
 const customers = require('../routes/customers')
 const genres = require('../routes/genres')
 const movies = require('../routes/movies')
+const rentals = require('../routes/rentals')
 
 module.exports = function(app) {
   app.use(express.json())
   app.use('/api/customers', customers)
   app.use('/api/genres', genres)
   app.use('/api/movies', movies)
+  app.use('/api/rentals', rentals)
 }
