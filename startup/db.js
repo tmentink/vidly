f8d08525e6aeb820e953e68be1b79e97b5cf8749
Trent Mentink <trentmentink@gmail.com>
2019-03-27 17:42:24 -0700

    Fixed mongoose deprecation and jest warnings
    Added jest.config.js to manually set the testEnvironment to node as recommended by mongoose's docs.
    Updated startup/db.js to include the recommended options when connecting to the database.

--- startup/db.js
+++ startup/db.js
@@ -4,5 +4,7 @@ const mongoose = require('mongoose')
 
 module.exports = function() {
   const db = config.get('db')
-  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`))
+  mongoose
+    .connect(db, { useNewUrlParser: true, useCreateIndex: true })
+    .then(() => winston.info(`Connected to ${db}...`))
 }
