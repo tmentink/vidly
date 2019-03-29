ec9973c3bbb7abb3b55d4686496c2acc7c21b447
Trent Mentink <trentmentink@gmail.com>
2019-03-29 12:16:18 -0700

    Created both integration and unit tests for middleware/auth.js

---
+++ tests/integration/middleware/auth.test.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-return-await */
+const request = require('supertest')
+const { Genre } = require('../../../models/genre')
+const { User } = require('../../../models/user')
+
+let server
+let token
+
+describe('auth middleware', () => {
+  beforeEach(() => {
+    server = require('../../../server')
+    token = new User({ isAdmin: true }).generateAuthToken()
+  })
+
+  afterEach(async () => {
+    await Genre.deleteMany({})
+    await server.close()
+  })
+
+  const exec = async () => {
+    return await request(server)
+      .post('/api/genres')
+      .set('x-auth-token', token)
+      .send({ name: 'name1' })
+  }
+
+  it('should return 401 if no token is provided', async () => {
+    token = ''
+
+    const res = await exec()
+
+    expect(res.status).toBe(401)
+  })
+
+  it('should return 400 if token is invalid', async () => {
+    token = 'a'
+
+    const res = await exec()
+
+    expect(res.status).toBe(400)
+  })
+
+  it('should return 200 if token is valid', async () => {
+    const res = await exec()
+
+    expect(res.status).toBe(200)
+  })
+})
