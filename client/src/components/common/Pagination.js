e1d53de3706325a793ab99c9ca76f49b5ca1ac5b
Trent Mentink <trentmentink@gmail.com>
2019-05-14 12:09:48 -0700

    Created common pagination component along with paginate util function

---
+++ client/src/components/common/Pagination.js
@@ -0,0 +1,39 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import _ from 'lodash'
+import PropTypes from 'prop-types'
+
+const Pagination = props => {
+  const { currentPage, itemsCount, onPageChange, pageSize } = props
+
+  const pagesCount = Math.ceil(itemsCount / pageSize)
+  if (pagesCount === 1) return null
+
+  const pages = _.range(1, pagesCount + 1)
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map(page => (
+          <li
+            key={page}
+            className={page === currentPage ? 'page-item active' : 'page-item'}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+Pagination.propTypes = {
+  currentPage: PropTypes.number.isRequired,
+  itemsCount: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  pageSize: PropTypes.number.isRequired,
+}
+
+export default Pagination
