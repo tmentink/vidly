0fee79862b0187268c4d41fb9f325b39ff85a51e
Trent Mentink <trentmentink@gmail.com>
2019-05-14 07:41:51 -0700

    Setup client for development with bootstrap and font-awesome

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './components/App/App'
+import App from './components/App'
 import * as serviceWorker from './serviceWorker'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
