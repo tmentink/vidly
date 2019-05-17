358df48636cdd138873cad505990812341350935
Trent Mentink <trentmentink@gmail.com>
2019-05-17 14:59:59 -0700

    Added a search box to the Movies component

--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import ListGroup from './common/ListGroup'
 import MoviesTable from './MoviesTable'
 import Pagination from './common/Pagination'
+import SearchBox from './common/SearchBox'
 import { getGenres } from '../services/genreService'
 import { getMovies } from '../services/movieService'
 import { paginate } from '../utils/paginate'
@@ -13,6 +15,7 @@ class Movies extends Component {
     genres: [],
     movies: [],
     pageSize: 4,
+    searchQuery: '',
     selectedGenre: null,
     sortColumn: { path: 'title', order: 'asc' },
   }
@@ -22,13 +25,19 @@ class Movies extends Component {
     const { data: movies } = await getMovies()
 
     this.setState({
-      genres: [{ name: 'All genres', _id: null }, ...genres],
+      genres: [{ name: 'All genres', _id: '' }, ...genres],
       movies,
     })
   }
 
   getFilteredMovies() {
-    const { movies, selectedGenre } = this.state
+    const { movies, searchQuery, selectedGenre } = this.state
+
+    if (searchQuery)
+      return movies.filter(m =>
+        m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+      )
+
     return selectedGenre && selectedGenre._id
       ? movies.filter(m => m.genre._id === selectedGenre._id)
       : movies
@@ -46,7 +55,7 @@ class Movies extends Component {
   }
 
   handleGenreSelect = genre => {
-    this.setState({ currentPage: 1, selectedGenre: genre })
+    this.setState({ currentPage: 1, searchQuery: '', selectedGenre: genre })
   }
 
   handleLike = movie => {
@@ -61,6 +70,10 @@ class Movies extends Component {
     this.setState({ currentPage: page })
   }
 
+  handleSearch = query => {
+    this.setState({ currentPage: 1, searchQuery: query, selectedGenre: null })
+  }
+
   handleSort = sortColumn => {
     this.setState({ sortColumn })
   }
@@ -70,6 +83,7 @@ class Movies extends Component {
       currentPage,
       genres,
       pageSize,
+      searchQuery,
       selectedGenre,
       sortColumn,
     } = this.state
@@ -88,9 +102,15 @@ class Movies extends Component {
           />
         </div>
         <div className="col-md-9">
-          <p className="mb-3 mt-3 mt-md-0">
-            Showing {filtered.length} movies in the database
-          </p>
+          <div className="d-flex my-3 mt-md-0">
+            <p className="m-0 align-self-end">
+              Showing {filtered.length} movies in the database
+            </p>
+            <Link to="/movies/new" className="btn btn-primary ml-auto">
+              New Movie
+            </Link>
+          </div>
+          <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
             movies={movies}
             onDelete={this.handleDelete}
